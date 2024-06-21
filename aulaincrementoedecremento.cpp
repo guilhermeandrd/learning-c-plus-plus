b1198372cd5ca9f5a4fc93b5012c19db8e2bc221
NicolyDev <nickyandrade331@gmail.com>
2024-06-21 08:25:24 -0300

    configuração do git ignore pt2

---
+++ aulaincrementoedecremento.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int n1, n2;
+
+    n1=15;
+    n2=10;
+
+    cout << n1 << "\n\n";
+    //n1=n1+5;   
+    //n1 += 1;
+    //n1 -=1;
+    //n1 *= 2;
+    //n1 /=2;
+    //n1 ++;
+        //nesse caso ele primeiro assume o valor do número e depois incrementa
+    //++ n2;
+        //ja nesse caso ele ja incrementa ao proprio valor do numeroo
+    //n1--;
+    //--n1;
+    n1*=2;
+        cout << n1 << "\n\n";
+
+
+
+    return 0;   
+}
