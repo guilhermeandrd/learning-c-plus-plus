e3ded5d60e3dbbb36eef2f82ac9751ddfc80ea8a
NicolyDev <nickyandrade331@gmail.com>
2024-06-21 19:03:19 -0300

    configurei gitignore e fiz mais uma aula
    consegui colocar o gitignore pra ignorar o arquivo compilado, adicionando um nome padrão para o arquivo e sempre usando ele.
    também fiz uma aula de if/else pt1

---
+++ aulaifelse1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n1=10;
+    int n2=50;
+    int sol=0;
+    int sol2=5;
+    char opc='s';
+
+    /*if(sol){
+        cout << "vou ao clube";
+    }else{
+        cout << "vou ao cinema";
+    }*/
+
+    //true = 1
+    //false = 0
+    //ent basca colocar o nome da variável
+    if(sol2>=10){
+        cout << "vou ao clube";
+    }else{
+        cout << "vou ao cinema";
+    }
+    //testes lógicos 
+    //<,>,>=,<=,!=
+    //&&, ||, !
+    return 0;
+}
